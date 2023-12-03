c4c94489a75ea392cd0d955fb9e8875c51fdabe9
deivid how <vvdoxi@gmail.com>
2023-12-03 18:57:59 -0500

    <added functions to the javascript file>

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,26 @@
 var topics= ['HTML', 'CSS', 'GIT', 'JavaScript'];
-
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+    function listtopics() {
 for (var x = 0; x < topics.length; x++) {
     console.log(topics[x]);
 }
+}
 
-/*
-if (topic === 'HTML') {
+function selecttopic (){
+if (randomTopic === 'HTML') {
     console.log("let's study HTML!");
-} else if (topic === 'CSS') {
+} else if (randomTopic === 'CSS') {
     console.log("let's study CSS!");
-} else if (topic ==='Git') {
+} else if (randomTopic ==='GIT') {
     console.log("let's study git!");
-} else if (topic === 'javascript') {
-    console.log("let's study javascript!");
+} else if (randomTopic === 'JavaScript') {
+    console.log("let's study JavaScript!");
 } else {
     console.log('please try again!');
 }
-*/
+}
+
+console.log('Here are the topics we learned through Prework:');
+listtopics()
+console.log('Which topic should we study first?');
+selecttopic()
